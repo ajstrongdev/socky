2a51899c7c9ea3f1cbd395543c63af200c14d7b2
ajstrongdev <ajstrongdev@proton.me>
2025-03-29 17:43:00 +0000

    Working auth
    Sign up and route protecting middleware still required

--- server/index.ts
+++ server/index.ts
@@ -17,7 +17,7 @@ io.on("connection", (socket) => {
   console.log("A user connected");
   
   socket.on("chat message", (msg) => {
-    io.emit("chat message", msg); // Broadcast message to all clients
+    io.emit("chat message", "Username: " + msg); // Broadcast message to all clients
   });
 
   socket.on("disconnect", () => {
